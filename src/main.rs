7db80f004eb83f9ea6be873daf211338c9ab0388
Brandon <brandondong96@gmail.com>
2023-08-31 23:16:43 -0700

    Print convergence change

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Args {
     soldiers: u32,
     #[arg(long, default_value_t = 0)]
     scoring: u8,
-    #[arg(long, default_value_t = 0.5)]
+    #[arg(long, default_value_t = 0.1)]
     mutation: f64,
 }
 
@@ -39,12 +39,24 @@ fn main() {
         scoring,
         args.mutation,
     );
+    let mut prev_best: Option<Vec<u32>> = None;
     for generation in 1..=args.generations {
         let results = ga.run_generation();
         let best = &results[0];
+        let change = match prev_best {
+            None => String::from("N/A"),
+            Some(i) => i
+                .iter()
+                .zip(&best.details.soldier_distribution)
+                .map(|(i, j)| i.abs_diff(*j))
+                .sum::<u32>()
+                .to_string(),
+        };
+
         println!(
-            "Generation {generation} winner: score={}, strategy={:?}",
-            best.score, best.details.soldier_distribution
+            "Generation {generation} winner: score={}, strategy={:?}, change={}",
+            best.score, best.details.soldier_distribution, change
         );
+        prev_best = Some(best.details.soldier_distribution.clone());
     }
 }
